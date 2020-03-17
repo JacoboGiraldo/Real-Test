4827a61eb670379a71b25ea92d060c55593c964e
Juan Jacobo Giraldo Rodriguez <juanjacobogiraldorodriguez@Juans-MacBook-Air-2.local>
2020-03-17 20:45:24 -0300

    First Solid Commit

---
+++ Proyect1Finalle/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Proyect1Finalle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Jacobo, Happy Quarantine!");
+        }
+    }
+}
